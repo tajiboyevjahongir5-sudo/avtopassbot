02ae8550e331ed3d871828218575f1f8a7a0841a
jahongirsteam1-ux <jahongirsteam1@gmail.com>
2026-06-16 13:09:41 +0500

    Update BOT_TOKEN in server.py

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ API_HASH = os.getenv("API_HASH", "your_api_hash")
 DATA_DIR = Path(os.getenv("DATA_DIR", "data"))
 DATA_DIR.mkdir(exist_ok=True)
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8881052991:AAFop1tZG0q4s8vnIkK76GSHCwE9X5qp9aM")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8603403010:AAFjbwFfZrtuVvhGMCSCw8-cY_ezUCTu6tE")
 MINI_APP_URL = os.getenv("MINI_APP_URL", "https://jahongirsteam1-ux.github.io/tezlashtiramiz/")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://forwardbot-production-1f08.up.railway.app")
 
